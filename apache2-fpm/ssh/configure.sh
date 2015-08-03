d053f24a8323eac0c02742a0987b162d4b7d137b
Gaurav Goyal <gaurav@Gauravs-Air.local.lan>
2015-08-03 23:18:05 +0530

    Complete mac docker setup

--- apache2-fpm/ssh/configure.sh
+++ apache2-fpm/ssh/configure.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+sed -ri -e "s/^upload_max_filesize.*/upload_max_filesize = ${PHP_UPLOAD_MAX_FILESIZE}/" \
+    -e "s/^post_max_size.*/post_max_size = ${PHP_POST_MAX_SIZE}/" /etc/php5/fpm/php.ini
+sed -i 's/;listen = \/var\/run\/php5-fpm.sock/listen = \/var\/run\/php5-fpm.sock/' /etc/php5/fpm/pool.d/www.conf && sed -i 's/\;listen.mode/listen.mode/' /etc/php5/fpm/pool.d/www.conf && sed -i 's/expose_php.*/expose_php = off/g' /etc/php5/fpm/php.ini && sed -i 's/allow_url_fopen.*/allow_url_fopen = off/g' /etc/php5/fpm/php.ini
+sed -i "s/export APACHE_RUN_GROUP=www-data/export APACHE_RUN_GROUP=staff/" /etc/apache2/envvars
+sed -i 's/listen\.group = www-data/listen\.group = staff/' /etc/php5/fpm/pool.d/www.conf
+
+chown -R www-data:staff /var/www
+chown -R www-data:staff /var/log/apache2
+source /etc/apache2/envvars
+
 if [ "${AUTHORIZED_KEYS}" != "**None**" ]; then
     echo "=> Found authorized keys"
     mkdir -p /root/.ssh
